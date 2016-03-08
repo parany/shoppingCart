e9efd8778b6d76bca100d76597d6b022a224ecc6
RAMILISON Andrianandraina Andriamparany <ramilison.andriamparany@gmail.com>
2016-03-08 16:32:39 +0300

    Fix IoC issue

--- ShoppingCart.FrontOffice/App_Start/UnityConfig.cs
+++ ShoppingCart.FrontOffice/App_Start/UnityConfig.cs
@@ -1,7 +1,10 @@
+using System.Data.Entity;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Practices.Unity;
+using ShoppingCart.Controllers;
+using ShoppingCart.Models;
 using ShoppingCart.Models.Models.Shopping;
 using ShoppingCart.Models.Models.User;
 using ShoppingCart.Models.Repositories.Concrete;
@@ -10,19 +13,23 @@ using Unity.Mvc5;
 
 namespace ShoppingCart
 {
-	public static class UnityConfig
-	{
-		public static void RegisterComponents()
-		{
-			var container = new UnityContainer();
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
 
-			// register all your components with the container here
-			// it is NOT necessary to register your controllers
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
 
-			container.RegisterType<IGenericRepository<Product>, GenericRepository<Product>>();
-			container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>();
+            container.RegisterType<IGenericRepository<Product>, GenericRepository<Product>>();
+            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>();
+            container.RegisterType<DbContext, ApplicationDbContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<UserManager<ApplicationUser>>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>(new HierarchicalLifetimeManager());
+            container.RegisterType<AccountController>(new InjectionConstructor());
 
-			DependencyResolver.SetResolver(new UnityDependencyResolver(container));
-		}
-	}
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
 }

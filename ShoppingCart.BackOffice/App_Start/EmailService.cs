95252ea5b5457eea01bdd83a83a33cf18a70dc1a
RAMILISON Andrianandraina Andriamparany <ramilison.andriamparany@gmail.com>
2016-03-08 22:27:44 +0300

    Code cleanup

---
+++ ShoppingCart.BackOffice/App_Start/EmailService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.BackOffice
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+    }
+
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+
+    // Configure the application sign-in manager which is used in this application.
+}

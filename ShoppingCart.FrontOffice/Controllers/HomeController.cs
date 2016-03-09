03ba8cd0179b1444561ad62bd13e6ba00d5923b1
verethragna01 <rtsiry@live.fr>
2016-03-09 16:37:18 +0300

    Category Page

--- ShoppingCart.FrontOffice/Controllers/HomeController.cs
+++ ShoppingCart.FrontOffice/Controllers/HomeController.cs
@@ -1,22 +1,32 @@
 ﻿using ShoppingCart.Models.Models.Entities;
 using ShoppingCart.Models.Repositories.Interface;
 using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ShoppingCart.Controllers
 {
     public class HomeController : Controller
     {
-        private IGenericRepository<Product> ProductRepository { get; } 
+        private IGenericRepository<Product> ProductRepository { get; }
+        public int PageSize = 10;
 
         public HomeController(IGenericRepository<Product> productRepository)
         {
             ProductRepository = productRepository;
         }
 
-        public ActionResult Index()
+        public ViewResult Index(int page = 1)
         {
-            var products = ProductRepository.GetAll();
-            return View();
+            IList<Product> products = ProductRepository.GetAll();
+            return View(products);
+            /*
+            return View(products
+                .OrderBy(p => p.Id)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize)
+               );
+               */
         }
 
         public ActionResult About()

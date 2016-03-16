574786a6bdbcea9b84e44991056e321a361259aa
verethragna01 <rtsiry@live.fr>
2016-03-16 14:52:59 +0300

    Lambda expression generator - Issue #1: convert to bool

---
+++ SharedFunctions/LambdaGenerator/ProductExpression.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCart.Models.Models.Entities;
+using Simpro.Expr;
+
+namespace SharedFunctions.LambdaGenerator
+{
+    public class ProductExpression
+    {
+        
+        private ExprParser _Expression;
+        public ProductExpression()
+        {
+            _Expression = new ExprParser();
+        }
+
+        public LambdaExpression Compare(string variable, string name, string category, decimal price)
+        {
+            return _Expression.Parse(
+                variable + "=>(" + 
+                "(" + name + " != null && !" + name + ".Equals('')) ? "+ variable + ".Name.Equals(" + name +") : true)" +
+                "&& ((" + category + " != null && !" + category + ".Equals('')) ? " + variable + ".Category.Name.Equals(" + category + ") : true)" +
+                "&& ((" + price + " > 0) ? " + variable + ".Price == " + price + " : true)" + 
+                ")"
+                );
+        }
+    }
+}
--- ShoppingCart.FrontOffice/Controllers/ProductsController.cs
+++ ShoppingCart.FrontOffice/Controllers/ProductsController.cs
@@ -7,10 +7,11 @@ using System.Linq.Expressions;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using SharedFunctions.LambdaGenerator;
 using ShoppingCart.Models;
 using ShoppingCart.Models.Models.Entities;
 using ShoppingCart.Models.Repositories.Interface;
-using ShoppingCart.ViewModels;
+using ShoppingCart.ViewModels;
 
 namespace ShoppingCart.Controllers
 {
@@ -31,19 +32,27 @@ namespace ShoppingCart.Controllers
 
 
         // GET: Products
-        public ActionResult List(string name = "", int ID = 0, decimal price= 0, string category = "")
+        public ActionResult List(string name = "", decimal price= 0, string category = "")
         {
-            /*
-            Func<Product, bool> condition = p =>
+            ProductDetailViewModel productsViewModel;
+            ProductExpression prExp = new ProductExpression();
+            Expression exp = prExp.Compare("p", name, category, price);
+
+
+            productsViewModel = new ProductDetailViewModel()
             {
-                return (string.IsNullOrEmpty(name) || p.Name.Equals(name))
-                       && (string.IsNullOrEmpty(name) || p.Name.Equals(name));
-            }
-            ProductDetailViewModel productList = new ProductDetailViewModel()
-            {
-                Products = ProductRepository.GetList(Expression)
-            }*/
-            return View();
+
+                //Products = ProductRepository.GetList(p =>
+                //                                          ((name != null && !name.Equals("")) ? p.Name.Equals(name) : true)
+                //                                          && ((category != null && !category.Equals("")) ? p.Category.Name.Equals(category) : true)
+                //                                          && ((price > 0) ? p.Price == price : true)
+                //                                          )
+
+
+                Products = ProductRepository.GetList(exp)
+
+            };
+            return View(productsViewModel);
         }
 
         // GET: Products/Details/5

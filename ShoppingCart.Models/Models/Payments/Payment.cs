5cd6297bad1243bb18c60f7221d62c05c6039b70
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2016-04-05 09:12:54 +0300

    Added inheritance between Payment and BaseObject

--- ShoppingCart.Models/Models/Payments/Payment.cs
+++ ShoppingCart.Models/Models/Payments/Payment.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using ShoppingCart.Models.Models.Entities;
 
 namespace ShoppingCart.Models.Models.Payments
 {
     [XmlInclude(typeof(Mvola))]
     [XmlInclude(typeof(Check))]
     [XmlInclude(typeof(PaymentCash))]
-    public abstract class Payment
+    public abstract class Payment : BaseObject
     {
         public string Name { get; set; }
     }

5dd953cf2f0968cc090da8e30aaa5856e74c9e3f
Gibass <rnmg.gino@gmail.com>
2016-04-13 15:14:26 +0300

    Achat Product

---
+++ ShoppingCart.BackOffice/Controllers/CheckoutController.cs
@@ -0,0 +1,24 @@
+﻿using ShoppingCart.CommonController.Infrastructure.Abstract;
+using ShoppingCart.Controllers;
+using ShoppingCart.Models.Models.Entities;
+using ShoppingCart.Models.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCart.BackOffice.Controllers
+{
+    public class CheckoutController : BasicCheckoutController
+    {
+        public CheckoutController(IGenericRepository<Cart> cartRepo,
+                                  IGenericRepository<Product> productRepo,
+                                  IGenericRepository<CartLine> cartlineRepo,
+                                  IGenericRepository<ShippingDetail> shipRepo,
+                                  IOrderProcessor proc)
+            : base(cartRepo, productRepo, cartlineRepo, shipRepo, proc)
+        {
+        }
+    }
+}

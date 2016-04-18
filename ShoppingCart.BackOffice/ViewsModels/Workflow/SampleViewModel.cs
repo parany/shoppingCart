ec7aaa6d9b60779a9eee5a8f8007a580bfb33324
verethragna01 <rtsiry@live.fr>
2016-04-18 15:39:16 +0300

    Basics workflow

---
+++ ShoppingCart.BackOffice/ViewsModels/Workflow/SampleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShoppingCart.CommonController.Tools;
+using ShoppingCart.Models.Models.Entities;
+
+namespace ShoppingCart.BackOffice.ViewsModels.Workflow
+{
+    public class SampleViewModel
+    {
+        public class BoxContent
+        {
+            public Cart Cart { get; set; }
+            public IList<CartProcessTree.NodeObject> Descriptions { get; set; }
+            public IList<CartProcessTree.NodeObject> Options { get; set; }
+        }
+
+        public IList<BoxContent> boxes { get; set; }
+    }
+}
---
+++ ShoppingCart.CommonController/Tools/CartProcessTree.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Xml;
+
+namespace ShoppingCart.CommonController.Tools
+{
+    public class CartProcessTree : XmlHandler
+    {
+        public CartProcessTree(string filePath): base(filePath)
+        {
+
+        }
+
+        public class NodeObject
+        {
+            public string label { get; set; }
+            public string type { get; set; }
+        }
+        // Get all informations for the stage
+
+        public IList<NodeObject> Descriptions(string path)
+        {
+            XmlNodeList nodes = DirectPathNode(path).ChildNodes;
+            IList<NodeObject> des = new List<NodeObject>();
+            foreach (XmlNode node in nodes)
+            {
+                if (!node.Name.Equals("Options"))
+                {
+                    des.Add(new NodeObject()
+                    {
+                        label = node.Name,
+                        type = node.Value
+                    });
+                }
+            }
+
+            return des;
+        }
+
+        public IList<NodeObject> ForwardOptions(string path)
+        {
+            
+            IList<NodeObject> des = new List<NodeObject>();
+
+            if (DirectPathNode(path + "/Options") != null)
+            {
+                XmlNodeList nodes = DirectPathNode(path + "/Options").ChildNodes;
+                foreach (XmlNode node in nodes)
+                {
+                    des.Add(new NodeObject()
+                    {
+                        label = node.Value,
+                        type = node.Name
+                    });
+                }
+            }
+
+            return des;
+        }
+
+        public bool TreeState(string path)
+        {
+            return DirectPathNode(path).SelectSingleNode("Options").HasChildNodes;
+        }
+
+        public IList<NodeObject> RowllingBack(string path)
+        {
+            XmlNodeList nodes = DirectPathNode(path).ParentNode.ParentNode.ChildNodes;
+            IList<NodeObject> des = new List<NodeObject>();
+            foreach (XmlNode node in nodes)
+            {
+                des.Add(new NodeObject()
+                {
+                    label = node.Name,
+                    type = node.Value
+                });
+            }
+
+            return des;
+        }
+
+        public string CurrentTreeState(string path)
+        {
+            return DirectPathNode(path).Name;
+        }
+    }
+}

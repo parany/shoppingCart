62d721f826c514edb70e77147ea9400c24bf4dc3
Gibass <rnmg.gino@gmail.com>
2016-04-25 09:44:01 +0300

    change Log File name

---
+++ ShoppingCart.Models/Tracking/ChangeTracking.cs
@@ -0,0 +1,21 @@
+﻿using ShoppingCart.Models.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models.Log
+{
+    public class ChangeTracking : BaseObject
+    {
+        public string ClassName { get; set; }
+        public string PropertyName { get; set; }
+        public Guid PrimaryKey { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public string Description { get; set; }
+        public DateTime DateChanged { get; set; }
+    }
+}
+

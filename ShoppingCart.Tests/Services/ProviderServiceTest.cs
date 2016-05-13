7794924bdbbefa3227d444aa1e0dd0e3feb32774
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2016-05-13 11:59:02 +0300

    Added ProviderServiceTest and a first test for AddProvider

---
+++ ShoppingCart.Tests/Services/ProviderServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ShoppingCart.Models.Repositories.Interface;
+using ShoppingCart.Models.Models.Entities;
+using ShoppingCart.Models.ViewModels;
+using ShoppingCart.Services.Implementation;
+
+namespace ShoppingCart.Tests.Services
+{
+    [TestClass]
+    public class ProviderServiceTest
+    {
+        [TestMethod]
+        public void AddProvider_CallsProviderRepositoryAdd_WhenCalledWithProviderViewModel()
+        {
+            var providerRepository = new Mock<IGenericRepository<Provider>>();
+            var providerViewModel = new ProviderViewModel();
+            providerViewModel.PaymentMethods = new String[0];
+            providerRepository.Setup(mock => mock.Add(It.IsAny<Provider>()));
+            var providerService = new ProvidersService(providerRepository.Object);
+            providerService.AddProvider(providerViewModel);
+            providerRepository.Verify(mock => mock.Add(It.IsAny<Provider>()));
+        }
+    }
+}
